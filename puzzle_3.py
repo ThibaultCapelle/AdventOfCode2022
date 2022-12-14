b96c3100e177ca6a06957b6635674007d3e74cdb
Thibault Capelle <capelle.thibault@protonmail.com>
2022-12-14 23:06:44 +0100

    puzzle 3-2

--- puzzle_3.py
+++ puzzle_3.py
@@ -6,27 +6,29 @@ Created on Tue Dec 13 23:16:10 2022
 @author: usera
 """
 
-import os
 import numpy as np
 filename='input_3'
 
 with open(filename, 'r') as f:
     lines=f.readlines()
-choice_value=dict({'X':1,'Y':2,'Z':3})
-opponent_dict=dict({'A':1,'B':2,'C':3})
-outcome_dict=dict({0:3,1:6,2:0})
 current_sum=0
-for line in lines:
-    line=line.rstrip('\n')
-    comp1, comp2 =line[:int(len(line)/2)], line[int(len(line)/2):]
-    for i, element in enumerate(comp1):
-        out=False
-        for j, element2 in enumerate(comp2):
+N=int(len(lines)/3)
+for i in range(N):
+    comp1, comp2, comp3=lines[3*i], lines[3*i+1], lines[3*i+2]
+    for element in comp1[:-1]:
+        out1, out2=False, False
+        for element2 in comp2[:-1]:
             if element==element2:
-                out=True
+                out1=True
                 break
-        if out:
+        if out1:
+            for element3 in comp3[:-1]:
+                if element3==element2:
+                    out2=True
+                    break
+        if out2:
             break
+    print([element, comp1, comp2, comp3])
     value=ord(element.lower())-ord('a')+1
     if element.isupper():
         value+=26
