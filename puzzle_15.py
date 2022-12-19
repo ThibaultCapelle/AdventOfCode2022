d7856cbe260ac039f38656c82806586fee1dd78c
Thibault Capelle <capelle_thibault@riseup.net>
2022-12-19 19:00:16 +0100

    puzzle 15-2

--- puzzle_15.py
+++ puzzle_15.py
@@ -26,31 +26,68 @@ for line in lines:
                     int(y_beacon[2:])])
 
 
-
-
-
-exclusion_lines=dict()
-for sensor, beacon in zip(sensors, beacons):
-    distance=np.abs(sensor[0]-beacon[0])+np.abs(sensor[1]-beacon[1])
-    print(sensor)
-    for y in range(sensor[1]-distance,
-                   sensor[1]+distance+1):
-        if not y in exclusion_lines.keys():
-            exclusion_lines[y]=[]
-        exclusion_lines[y].append([sensor[0]-distance+np.abs(y-sensor[1]),
-                   sensor[0]+distance-np.abs(y-sensor[1])])
-
-def check_a_line(y):
-    position_blocked=dict()
-    for item in exclusion_lines[y]:
-        x1, x2 =item
-        for i in range(x1, x2+1):
-            position_blocked[i]=True
-    for beacon in beacons:
-        if beacon[1]==y:
-            if beacon[0] in position_blocked.keys():
-                position_blocked.pop(beacon[0])
-    return len(position_blocked.keys())
-
-print(check_a_line(2000000))
         
+
+nodes=[]
+class Node:
+    
+    def __init__(self, sensor, beacon):
+        self.sensor=sensor
+        self.beacon=beacon
+        self.distance=np.abs(self.sensor[0]-self.beacon[0])+\
+            np.abs(self.sensor[1]-self.beacon[1])
+        point1, point2=([self.sensor[0], self.sensor[1]+self.distance],
+                        [self.sensor[0], self.sensor[1]-self.distance])
+        new_point1, new_point2=self.rotate(point1), self.rotate(point2)
+        self.range=[new_point2[0], new_point2[1],
+                    new_point1[0], new_point1[1]]
+    
+    def inside(self, point):
+        return point[0]>=self.range[0] and point[0]<=self.range[2] and\
+            point[1]>=self.range[1] and point[1]<=self.range[3]
+    
+    def in_range(self, point):
+        point=self.unrotate(point)
+        return (point[0]<=4000000 and point[0]>=0 
+                and point[1]<=4000000 and point[1]>=0)
+    
+    def rotate(self, point):
+        return [point[0]+point[1], point[1]-point[0]]
+    
+    def unrotate(self, point):
+        assert (point[0]+point[1])%2==0
+        assert (point[0]-point[1])%2==0
+        return [int((point[0]-point[1])/2), 
+                int((point[1]+point[0])/2)]
+    
+    def neghbours(self):
+        res=[[self.range[0]-1,i] for i in range(self.range[1],
+                                               self.range[3]+1)]
+        res+=[[self.range[2]+1,i] for i in range(self.range[1],
+                                               self.range[3]+1)]
+        res=[[i,self.range[1]-1] for i in range(self.range[0],
+                                               self.range[2]+1)]
+        res+=[[i,self.range[3]+1] for i in range(self.range[0],
+                                               self.range[2]+1)]
+        return res
+                   
+
+        
+for sensor, beacon in zip(sensors, beacons):
+    nodes.append(Node(sensor, beacon))
+for node in nodes:
+    for neighbour in node.neghbours():
+        out=False
+        for node_bis in nodes:
+            if node_bis.inside(neighbour):
+                out=True
+                break
+        if not out:
+            if node.in_range(neighbour):
+                x, y =node.unrotate(neighbour)
+                print(x*4000000+y)
+                break
+            
+            
+
+    
