56bde9049aaec68fd97db76f686b519957549da7
Thibault Capelle <capelle_thibault@riseup.net>
2022-12-19 15:00:39 +0100

    puzzle 15-1

--- puzzle_15.py
+++ puzzle_15.py
@@ -25,40 +25,32 @@ for line in lines:
     beacons.append([int(x_beacon[2:]),
                     int(y_beacon[2:])])
 
-xmin, xmax=(np.min([np.min([sensor[0] for sensor in sensors]),
-                    np.min([beacon[0] for beacon in beacons])]),
-            np.max([np.max([sensor[0] for sensor in sensors]),
-                    np.max([beacon[0] for beacon in beacons])]))
-ymin, ymax=(np.min([np.min([sensor[1] for sensor in sensors]),
-                    np.min([beacon[1] for beacon in beacons])]),
-            np.max([np.max([sensor[1] for sensor in sensors]),
-                    np.max([beacon[1] for beacon in beacons])]))
 
 
 
 
-
-extra=2000000
-beacon_map=np.zeros((xmax-xmin+2*extra, ymax-ymin+2*extra), dtype=np.int8)
-def change_coord(coords):
-    return [coords[0]-xmin+extra, coords[1]-ymin+extra]
-
+exclusion_lines=dict()
 for sensor, beacon in zip(sensors, beacons):
-    sensor, beacon=change_coord(sensor), change_coord(beacon)
-    beacon_map[sensor[0], sensor[1]]=1
-    beacon_map[beacon[0], beacon[1]]=-1
     distance=np.abs(sensor[0]-beacon[0])+np.abs(sensor[1]-beacon[1])
-    if (sensor[0]<distance or sensor[1]<distance
-        or sensor[0]+distance>=beacon_map.shape[0] or
-        sensor[1]+distance>=beacon_map.shape[1]):
-        print('problem')
-        print([sensor, beacon, distance])
-    print([sensor, distance])
-    for x in range(sensor[0]-distance,
-                   sensor[0]+distance+1):
-        for y in range(sensor[1]-distance+np.abs(x-sensor[0]),
-                   sensor[1]+distance+1-+np.abs(x-sensor[0])):
-            beacon_map[x,y]=2
+    print(sensor)
+    for y in range(sensor[1]-distance,
+                   sensor[1]+distance+1):
+        if not y in exclusion_lines.keys():
+            exclusion_lines[y]=[]
+        exclusion_lines[y].append([sensor[0]-distance+np.abs(y-sensor[1]),
+                   sensor[0]+distance-np.abs(y-sensor[1])])
 
-    
-    
+def check_a_line(y):
+    position_blocked=dict()
+    for item in exclusion_lines[y]:
+        x1, x2 =item
+        for i in range(x1, x2+1):
+            position_blocked[i]=True
+    for beacon in beacons:
+        if beacon[1]==y:
+            if beacon[0] in position_blocked.keys():
+                position_blocked.pop(beacon[0])
+    return len(position_blocked.keys())
+
+print(check_a_line(2000000))
+        
